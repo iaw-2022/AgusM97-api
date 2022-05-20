ef7eb84e1318498b8a4ddef6794beb6f3f298d03
AgusM97 <agus91997@gmail.com>
2022-05-19 23:52:46 -0300

    validacion user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,8 @@ const getUserByUsername = async (req, res) => {
     "SELECT id, username, email, picture, bio, created_at, updated_at  FROM users WHERE username = $1",
     [username]
   );
-  res.json(response.rows[0]);
+  if (response.rows.length == 0) res.status(404).send("User not found");
+  res.status(404).json(response.rows[0]);
 };
 
 const getUserById = async (req, res) => {
@@ -29,6 +30,7 @@ const getUserById = async (req, res) => {
     "SELECT id, username, email, picture, bio, created_at, updated_at  FROM users WHERE id = $1",
     [id]
   );
+  if (response.rows.length == 0) res.status(404).send("User not found");
   res.json(response.rows[0]);
 };
 
@@ -38,6 +40,7 @@ const getUserByEmail = async (req, res) => {
     "SELECT id, username, email, picture, bio, created_at, updated_at  FROM users WHERE email = $1",
     [email]
   );
+  if (response.rows.length == 0) res.status(404).send("User not found");
   res.json(response.rows[0]);
 };
 
@@ -45,7 +48,11 @@ const updateUserBio = async (req, res) => {
   const username = req.params.username;
   const bio = req.body.bio;
   const now = new Date();
-  const response = await pool.query(
+  const response = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  if (response.rows.length == 0) res.status(404).send("User not found");
+  await pool.query(
     "UPDATE users SET bio = $1, updated_at = $2 WHERE username = $3",
     [bio, now, username]
   );
@@ -58,7 +65,11 @@ const updateUserPicture = async (req, res) => {
   const username = req.params.username;
   const picture = req.body.picture;
   const now = new Date();
-  const response = await pool.query(
+  const response = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  if (response.rows.length == 0) res.status(404).send("User not found");
+  await pool.query(
     "UPDATE users SET picture = $1, updated_at = $2 WHERE username = $3",
     [picture, now, username]
   );
@@ -71,15 +82,37 @@ const createUser = async (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = await bcrypt.hash(req.body.password, 10);
-  const created_at = (updated_at = new Date());
-  const response = await pool.query(
-    "INSERT INTO users (username, password, email, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)",
-    [username, password, email, created_at, updated_at]
+  const now = new Date();
+
+  const response1 = await pool.query(
+    "SELECT * FROM users WHERE username = $1",
+    [username]
+  );
+  if (response1.rows.length != 0)
+    res.status(409).send("This username is already taken");
+
+  const response2 = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+  if (response2.rows.length != 0)
+    res
+      .status(409)
+      .send("There is already an account using this email address");
+
+  const response3 = await pool.query("SELECT password FROM users");
+  response3.rows.forEach((row) => {
+    if (bcrypt.compare(req.body.password, row.password))
+      res.status(409).send("This password is already taken");
+  });
+
+  await pool.query(
+    "INSERT INTO users (username, password, email, created_at, updated_at) VALUES ($1, $2, $3, $4, $4)",
+    [username, password, email, now]
   );
   res.json({
     message: "User Added Succefully",
     body: {
-      user: { username, email, password },
+      user: { username, email },
     },
   });
 };
