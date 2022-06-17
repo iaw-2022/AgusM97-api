013fc9d3c1ea5c59550470f14846993d0044cd69
AgusM97 <agus91997@gmail.com>
2022-06-17 19:52:10 -0300

    correccion

--- src/index.js
+++ src/index.js
@@ -11,8 +11,11 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(
+  express.json({
+    limit: "25mb",
+  })
+);
 
 //routes
 app.use(require("./routes/user.routes"));
